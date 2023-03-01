7afd8d82ad2eb5624f45b573cabc730797d3e995
alexbull77 <bularga.alexander@gmail.com>
2023-03-01 21:16:53 +0200

    -

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -28,5 +28,6 @@ router.register(r'products', ProductView, 'product-list')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls'))
     # path('api2/', include('blog_api.urls')),
 ]
