d5c8dabd418aec8ad43b4ddefb4cd8c08685711f
alexbull77 <98638738+alexbull77@users.noreply.github.com>
2023-03-24 11:39:51 +0200

    Delete backend directory

--- backend/products_api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import ProductModel, CategoryModel
-
-admin.site.register(CategoryModel)
-
-
-@admin.register(ProductModel)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'price', 'rating', 'stock')
